6f21e2168fbb07729840e3eb10cf0d6598fb09d4
shivkumar13 <shivkumar.ople97@gmail.com>
2022-02-01 23:56:32 +0530

    Separating logic with helper.go

---
+++ helper.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"strings"
+)
+
+func validateUserInputs(firstName string, lastName string, email string, userTickets uint) (bool, bool, bool) {
+
+	isValidName := len(firstName) >= 2 && len(lastName) >= 2
+	isValidEmail := strings.Contains(email, "@")
+	isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
+
+	return isValidEmail, isValidName, isValidTicketNumber
+}
